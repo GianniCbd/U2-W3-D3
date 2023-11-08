dba481444930520a7de929517631cb0286918d3c
GianniCbd <giannicabiddu@gmail.com>
2023-11-08 17:36:55 +0100

    U2-W3-D3

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
+window.onload = function () {
+  getBooks();
+};
+
 function getBooks() {
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((response) => response.json())
     .then((data) => displayBooks(data))
-    .catch((error) => console.error("Errore nella richiesta:", error));
+    .catch((error) => console.error("Error:", error));
 }
 
 function displayBooks(books) {
@@ -11,30 +15,41 @@ function displayBooks(books) {
   books.forEach((book) => {
     const card = document.createElement("div");
     card.classList.add("col-sm-12", "col-md-4", "col-lg-3", "col-12", "mb-4");
-
+    // cardBody
     const cardBody = document.createElement("div");
+    cardBody.className = "text-center";
     card.appendChild(cardBody);
-
+    // img
     const img = document.createElement("img");
     img.src = book.img;
+
     img.classList.add("card-img-top");
     cardBody.appendChild(img);
-
+    // titolo
     const h4 = document.createElement("h4");
     h4.textContent = book.title;
     cardBody.appendChild(h4);
-
+    // categoria
     const p = document.createElement("p");
-    p.textContent = "categoria: " + book.category;
+    p.textContent = "Price: " + book.price;
     cardBody.appendChild(p);
-
+    // bottone scarta
     const button = document.createElement("button");
     button.classList.add("btn", "btn-danger");
     button.textContent = "scarta";
+    button.onclick = function () {
+      cardBody.remove();
+    };
     cardBody.appendChild(button);
+    //aggiungi al carrello
+    const addToCartButton = document.createElement("button2");
+    addToCartButton.classList.add("btn", "btn-success");
+    addToCartButton.textContent = "Aggiungi al carrello";
+    addToCartButton.onclick = function () {
+      addToCart(book);
+    };
+    cardBody.appendChild(addToCartButton);
 
     bookList.appendChild(card);
   });
 }
-
-getBooks();
